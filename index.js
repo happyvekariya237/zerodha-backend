caed1380074d70eb85a7374f19569ff90c860c4d
happyvekariya237 <happyvekariya237@gmail.com>
2025-12-05 16:40:31 +0530

    donee

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 8080;
 const uri = process.env.MONGO_URL;
 
 // -------------------- FIXED CORS (PLACE ON TOP) --------------------
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174","https://zerodha-dashboard-rh4a.onrender.com","https://zerodha-backend-lobs.onrender.com/"];
 
 app.use(cors({
   origin: function(origin, callback){
@@ -38,6 +38,10 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/", authRoute);
+
+app.get("/",(req,res)=>{
+  res.send("hellow")
+})
 
 app.get("/allholding", async (req, res) => {
   let allholding = await holdingModel.find({});
